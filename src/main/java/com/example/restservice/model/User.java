cf7d6bd2edac417c0693bf11ff2d55a3f79210b9
DeepLake1 <saawkkaa@gmail.com>
2021-04-25 15:24:39 +0300

    init db

--- src/main/java/com/example/restservice/model/User.java
+++ src/main/java/com/example/restservice/model/User.java
@@ -3,10 +3,15 @@ package com.example.restservice.model;
 public class User {
     private long id;
     private String name;
+    private String email;
+    private long phoneNumber;
+    private Status status;
 
-    public User(long id, String name) {
+    public User(long id, String name, String email, long phoneNumber) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     public long getId() {
@@ -25,4 +30,19 @@ public class User {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
