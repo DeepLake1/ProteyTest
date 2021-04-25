fb8c7216a45abf608bb0adc287385d650566b7c4
DeepLake1 <saawkkaa@gmail.com>
2021-04-25 15:27:59 +0300

    init db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS user_status;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -10,28 +9,16 @@ CREATE TABLE users
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     name             VARCHAR                           NOT NULL,
     email            VARCHAR                           NOT NULL,
+    phone            VARCHAR                           NOT NULL,
     password         VARCHAR                           NOT NULL,
     registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
-CREATE TABLE user_roles
+CREATE TABLE user_status
 (
     user_id INTEGER NOT NULL,
-    role    VARCHAR,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    status    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, status),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-
-CREATE TABLE meals
-(
-    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    user_id     INTEGER   NOT NULL,
-    date_time   TIMESTAMP NOT NULL,
-    description TEXT      NOT NULL,
-    calories    INT       NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (user_id, date_time);
