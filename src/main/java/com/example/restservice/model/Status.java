9989a37d26c99cec670ed321564d65c95faf2f4a
DeepLake1 <saawkkaa@gmail.com>
2021-04-28 21:25:05 +0300

    Before @OneToOne changing_2

--- src/main/java/com/example/restservice/model/Status.java
+++ src/main/java/com/example/restservice/model/Status.java
@@ -23,7 +23,6 @@ public class Status {
     @Id
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-    @Column(name = "user_id", updatable = false, nullable = false)
     protected int id;
 
     @OneToOne
